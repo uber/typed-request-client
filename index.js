6c4fdcfba8e3c50f79ac598209f2fe583cc382d8
Kris Kowal <kris@uber.com>
2014-10-23 13:45:05 -0700

    Factor validating client out of typed client

--- index.js
+++ index.js
@@ -23,22 +23,11 @@ function TypedRequestClient(options) {
         request: options.request || globalRequest
     };
 
-    return StatsdClient(typedRequestClient, options);
+    return StatsdClient(ValidatingClient(typedRequestClient, options), options);
 
     function typedRequestClient(treq, opts, cb) {
-        var requestSchema = opts.requestSchema;
-        var responseSchema = opts.responseSchema;
 
-        var result = validateShape(treq, requestSchema);
-        if (result.type === 'error') {
-            result.error.treq = treq;
-            result.error.schema = requestSchema;
-
-            // TODO make this a better error.
-            return cb(result.error);
-        }
-
-        probedRequest(result.ok, reqOpts, onResponse);
+        probedRequest(treq, reqOpts, onResponse);
 
         function onResponse(err, tres) {
 
@@ -47,17 +36,8 @@ function TypedRequestClient(options) {
                 return cb(err);
             }
 
-            var result = validateShape(tres, responseSchema);
+            cb(null, tres);
 
-            if (result.type === 'error') {
-                result.error.tres = tres;
-                result.error.schema = responseSchema;
-
-                // TODO make this a better error.
-                return cb(result.error);
-            }
-
-            cb(null, result.ok);
         }
     }
 }
@@ -110,6 +90,43 @@ function StatsdClient(client, options) {
 
             cb(null, tres);
 
+        }
+    }
+}
+
+function ValidatingClient(client, options) {
+    return validatingClient;
+
+    function validatingClient(treq, opts, cb) {
+        var requestSchema = opts.requestSchema;
+        var responseSchema = opts.responseSchema;
+
+        var result = validateShape(treq, requestSchema);
+        if (result.type === 'error') {
+            result.error.treq = treq;
+            result.error.schema = requestSchema;
+
+            // TODO make this a better error.
+            return cb(result.error);
+        }
+
+        client(result.ok, opts, onResponse);
+
+        function onResponse(err, tres) {
+            if (err) {
+                return cb(err);
+            }
+            var result = validateShape(tres, responseSchema);
+
+            if (result.type === 'error') {
+                result.error.tres = tres;
+                result.error.schema = responseSchema;
+
+                // TODO make this a better error.
+                return cb(result.error);
+            }
+
+            cb(null, result.ok);
         }
     }
 }
